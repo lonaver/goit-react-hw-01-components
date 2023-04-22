6b7b0331619612190b01f1ca6fd19143e979b651
lonaver <ilonaver@ukr.net>
2023-04-22 14:10:29 +0300

    hw1 fixed styles transactions

--- src/components/TransactionData/TransactionData.jsx
+++ src/components/TransactionData/TransactionData.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import styles from './TransactionData.module.css';
 
 export const TransactionData = ({ type, amount, currency }) => {
   return (
-    <tr>
-      <td>{type}</td>
-      <td>{amount}</td>
-      <td>{currency}</td>
+    <tr className={styles.tr_item}>
+      <td className={styles.td_item}>{type}</td>
+      <td className={styles.td_item}>{amount}</td>
+      <td className={styles.td_item}>{currency}</td>
     </tr>
   );
 };
